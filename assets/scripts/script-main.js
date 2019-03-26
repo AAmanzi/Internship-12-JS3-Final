288dd120e7552a62b706ad642802439557e40747
AAmanzi <alnick.amanzi@gmail.com>
2019-03-26 17:34:11 +0100

    Added fetch from API and users page

--- assets/scripts/script-main.js
+++ assets/scripts/script-main.js
@@ -1,3 +1,5 @@
+let loggedIn = false;
+
 document.querySelector(".logo__container").addEventListener("click", () => {
   displayMainPage();
 });
@@ -8,6 +10,10 @@ document.querySelector(".button-full").addEventListener("click", () => {
 
 document.querySelector(".button-login").addEventListener("click", () => {
   displayLogin();
+});
+
+document.querySelector(".button-users").addEventListener("click", () => {
+  displayUsers();
 });
 
 function displayLogin(){
@@ -37,11 +43,69 @@ function displayMainPage(){
 
   document.querySelector(".login__container").classList.add("display-none");
   document.querySelector(".login__container").classList.remove("visible");
+
+  document.querySelector(".main__users").innerHTML = "";
 };
 
 function displayUsers(){
+  if(!loggedIn){
+    return false;
+  }
+  document.querySelector(".header__image__container").classList.add("display-none");
+  document.getElementsByTagName("main")[0].classList.add("display-none");
+  document.getElementsByTagName("footer")[0].classList.add("display-none");
+  document.querySelector(".login__container").classList.add("display-none");
+  document.querySelector(".login__container").classList.remove("visible");
 
+  let userPlaceholder = document.querySelector(".main__users");
+
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(allUsers => {
+    allUsers.forEach(user => {
+      userPlaceholder.innerHTML += 
+      `<div class="users__item">
+        <img class="item__img" src="./assets/images/test_image.jpg" alt="Offer" />
+        <img class="item__heart-shape" src="./assets/images/heart-shape.png" alt="Hearth Shape">
+        <img class="item__heart-full" src="./assets/images/heart.png" alt="Hearth Full">
+        <p class="item__img-description">${user.username}</p>
+        <p class="item__paragraph">
+            ${user.email}
+        </p>
+        <span class="item__price">${user.address.city}</span>
+      </div>`;
+    });
+  })
+  .then(() => {
+    let allUsersHTML = document.querySelectorAll(".users__item");
+
+    allUsersHTML.forEach(user => {
+      user.addEventListener("mouseover", function() {
+        let itemDescription = user.querySelector(".item__img-description");
+        itemDescription.classList.add("item__img-description-hover");
+      });
+
+      user.addEventListener("mouseout", function() {
+        let itemDescription = user.querySelector(".item__img-description");
+        itemDescription.classList.remove("item__img-description-hover");
+      });
+      
+      let itemHeart = user.querySelector(".item__heart-full");
+
+      itemHeart.addEventListener("click", function(event) {
+        itemHeart.classList.toggle("item__heart-full-click");
+        event.stopPropagation();
+      });
+
+      //item click event (posts)
+
+    });
+  });
 };
+
+function toggleLoginButton(){
+  document.querySelector(".button-full").classList.toggle("display-none");
+}
 
 function addUser(){
   let addUserForm = document.addUserForm;
@@ -97,6 +161,10 @@ function logIn(){
 
   if(anyErrors())
     return false;
+
+  loggedIn = true;
+  toggleLoginButton();
+  displayUsers();
 
   function anyErrors(){
     let error = document.querySelector(".login__error");
